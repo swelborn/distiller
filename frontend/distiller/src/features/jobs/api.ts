af008734cb9416420e6ddbc7fe2d183bb25ec53b
Sam Welborn <swelborn@lbl.gov>
2023-06-13 14:38:58 -0400

    update jobs api
    - include getJobs/getJob

--- frontend/distiller/src/features/jobs/api.ts
+++ frontend/distiller/src/features/jobs/api.ts
@@ -1,4 +1,4 @@
-import { IdType, JobType, Job } from '../../types';
+import { IdType, JobType, Job, JobsRequestResult } from '../../types';
 import { apiClient } from '../../client';
 
 export function createJob(
@@ -18,6 +18,61 @@ export function createJob(
     .post({
       url: 'jobs',
       json: payload,
+    })
+    .then((res) => res.json());
+}
+
+export function getJobs(
+  withScans?: boolean,
+  skip?: number,
+  limit?: number
+): Promise<JobsRequestResult> {
+  const params: any = {};
+  if (withScans !== undefined) {
+    params['with_scans'] = withScans;
+  }
+  if (skip !== undefined) {
+    params['skip'] = skip;
+  }
+  if (limit !== undefined) {
+    params['limit'] = limit;
+  }
+
+  return apiClient
+    .get({
+      url: 'jobs',
+      params,
+    })
+    .then((res) => {
+      return res.json().then((jobs) => {
+        let totalCount = -1;
+
+        const totalJobCountHeader = res.headers.get('x-total-count');
+        if (totalJobCountHeader != null) {
+          totalCount = Number.parseInt(totalJobCountHeader);
+        }
+
+        const jobsRequestResult = {
+          jobs,
+          totalCount,
+        };
+
+        return new Promise<JobsRequestResult>((resolve) => {
+          resolve(jobsRequestResult);
+        });
+      });
+    });
+}
+
+export function getJob(id: IdType, withScans?: boolean): Promise<Job> {
+  const params: any = {};
+  if (withScans !== undefined) {
+    params['with_scans'] = withScans;
+  }
+  return apiClient
+    .get({
+      url: `jobs/${id}`,
+      params,
     })
     .then((res) => res.json());
 }
