e5ad9585058f74f4edf33afbbcbc6146fc0b06f7
Sam Welborn <swelborn@lbl.gov>
2023-06-13 09:55:50 -0400

    update job crud
    - default behavior to not have scans in job
    - added utility function for getting scans
    - updated update_job to include updating job from a scan_id (adding scan to job)

--- backend/app/app/crud/job.py
+++ backend/app/app/crud/job.py
@@ -1,13 +1,23 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 from sqlalchemy import desc, or_, update
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, noload
 
 from app import models, schemas
+from app.crud import scan as scan_crud
 
 
-def get_job(db: Session, id: int):
-    return db.query(models.Job).filter(models.Job.id == id).first()
+def get_job(db: Session, id: int, with_scans: bool = False) -> Union[models.Job, None]:
+    query = db.query(models.Job)
+
+    if not with_scans:
+        query = query.options(noload(models.Job.scans))
+
+    return query.filter(models.Job.id == id).first()
+
+
+def get_scans_for_job(db: Session, job: models.Job):
+    return [scan_crud.get_scan(db, scan.id, with_jobs=False) for scan in job.scans]
 
 
 def get_job_by_slurm_id(db: Session, slurm_id: int):
@@ -18,21 +28,24 @@ def get_jobs(
     db: Session,
     skip: int = 0,
     limit: int = 100,
-    scan_id: int = None,
     slurm_id: int = None,
+    with_scans: bool = False,
 ):
     query = db.query(models.Job)
-    if scan_id is not None:
-        query = query.filter(models.Job.scan_id == scan_id)
 
     if slurm_id is not None:
         query = query.filter(models.Job.slurm_id == slurm_id)
+
+    if not with_scans:
+        query = query.options(noload(models.Job.scans))
 
     return query.order_by(desc(models.Job.id)).offset(skip).limit(limit).all()
 
 
 def create_job(db: Session, job: schemas.JobCreate):
-    db_job = models.Job(**job.dict())
+    _job = job.dict()
+    del _job["scan_id"]
+    db_job = models.Job(**_job)
     db.add(db_job)
     db.commit()
     db.refresh(db_job)
@@ -46,6 +59,7 @@ def update_job(
     statement = update(models.Job).where(models.Job.id == id)
 
     or_comparisons = []
+    updated = False
 
     if updates.state is not None:
         statement = statement.values(state=updates.state)
@@ -66,10 +80,27 @@ def update_job(
         or_comparisons.append(models.Job.elapsed != updates.elapsed)
         or_comparisons.append(models.Job.elapsed == None)
 
-    statement = statement.where(or_(*or_comparisons))
+    if updates.scan_id is not None:
+        scan = scan_crud.get_scan(db, updates.scan_id)
 
-    resultproxy = db.execute(statement)
-    updated = resultproxy.rowcount == 1
+        if scan is None:
+            raise Exception(f"Scan with id {updates.scan_id} does not exist.")
+
+        job = get_job(db, id, with_scans=True)
+        scans_updated = False
+
+        if job is not None and not any([s.id == updates.scan_id for s in job.scans]):
+            job.scans.append(scan)
+            scans_updated = True
+
+        updated = updated or scans_updated
+
+    if or_comparisons:
+        statement = statement.where(or_(*or_comparisons))
+
+        resultproxy = db.execute(statement)
+        updated = resultproxy.rowcount == 1 or updated
+
     db.commit()
 
-    return (updated, get_job(db, id))
+    return (updated, get_job(db, id, with_scans=False))
