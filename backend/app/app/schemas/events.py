1928000e6c49d0b8f56694ccf922424bbadca72e
Sam Welborn <swelborn@lbl.gov>
2023-06-13 09:47:37 -0400

    option scan, SubmitJobEvent

--- backend/app/app/schemas/events.py
+++ backend/app/app/schemas/events.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 from app.schemas.job import Job
 from app.schemas.scan import Scan
 
+from typing import Optional
+
 
 # Has to go in separate module rather the job.py because of circular import.
 class SubmitJobEvent(BaseModel):
     job: Job
-    scan: Scan
+    scan: Optional[Scan]
 
 
 class RemoveScanFilesEvent(BaseModel):
