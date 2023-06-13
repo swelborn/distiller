a27286c3d66c98f1ee9f63267a909272e84cbda6
Sam Welborn <swelborn@lbl.gov>
2023-06-13 10:04:23 -0400

    update jobs endpoint
    - create job significantly changed
    -- no longer sends scan update event, will fix this later
    - added with_scans to get methods
    - added /jobs/{id}/scans endpoint
    - removed scanupdateevent from patch method. will have to fix later on kafka consumer/websocket

--- backend/app/app/api/api_v1/endpoints/jobs.py
+++ backend/app/app/api/api_v1/endpoints/jobs.py
@@ -20,38 +20,44 @@ router = APIRouter()
     response_model=schemas.Job,
     dependencies=[Depends(oauth2_password_bearer_or_api_key)],
 )
-async def create_job(job: schemas.JobCreate, db: Session = Depends(get_db)):
-    scan = scan_crud.get_scan(db, job.scan_id)
-    if scan is None:
-        raise HTTPException(status_code=404, detail="Scan not found")
+async def create_job(job_create: schemas.JobCreate, db: Session = Depends(get_db)):
+    scan_id = job_create.scan_id
+    job = crud.create_job(db=db, job=job_create)
 
-    job = crud.create_job(db=db, job=job)
+    if scan_id is not None:
+        job_update = schemas.JobUpdate(scan_id=scan_id)
+        (updated, job) = crud.update_job(db, cast(int, job.id), job_update)
 
-    scan = schemas.Scan.from_orm(scan)
-    job = schemas.Job.from_orm(job)
-
-    await send_submit_job_event_to_kafka(SubmitJobEvent(scan=scan, job=job))
-
-    jobs = crud.get_jobs(db, scan_id=job.scan_id)
-    await send_scan_event_to_kafka(ScanUpdateEvent(id=job.scan_id, jobs=jobs))
+    db.refresh(job)
+    job = crud.get_job(db, job.id, with_scans=True)
+    scans_without_jobs = crud.get_scans_for_job(db, job)
+    job.scans = scans_without_jobs
+    if job.scans:
+        scan = job.scans[0]
+    else:
+        scan = None
+    await send_submit_job_event_to_kafka(SubmitJobEvent(job=job, scan=scan))
 
     return job
 
 
 @router.get(
     "",
-    response_model=List[schemas.Scan],
+    response_model=List[schemas.Job],
     dependencies=[Depends(oauth2_password_bearer_or_api_key)],
 )
 def read_jobs(
     skip: int = 0,
     limit: int = 100,
-    scan_id: int = -1,
+    with_scans: bool = False,
     db: Session = Depends(get_db),
 ):
-    jobs = crud.get_jobs(db, skip=skip, limit=limit, scan_id=scan_id)
+    db_jobs = crud.get_jobs(db, skip=skip, limit=limit, with_scans=with_scans)
+    if with_scans:
+        for job in db_jobs:
+            job.scans = crud.get_scans_for_job(db, job)
 
-    return jobs
+    return db_jobs
 
 
 @router.get(
@@ -59,12 +65,29 @@ def read_jobs(
     response_model=schemas.Job,
     dependencies=[Depends(oauth2_password_bearer_or_api_key)],
 )
-def read_job(id: int, db: Session = Depends(get_db)):
-    db_job = crud.get_job(db, id=id)
+def read_job(id: int, with_scans: bool = False, db: Session = Depends(get_db)):
+    db_job = crud.get_job(db, id=id, with_scans=with_scans)
     if db_job is None:
         raise HTTPException(status_code=404, detail="Job not found")
 
+    if with_scans:
+        db_job.scans = crud.get_scans_for_job(db, db_job)
+
     return db_job
+
+
+@router.get(
+    "/{id}/scans",
+    response_model=List[schemas.Scan],
+    response_model_by_alias=False,
+    dependencies=[Depends(oauth2_password_bearer_or_api_key)],
+)
+def read_job_scans(id: int, db: Session = Depends(get_db)):
+    db_job = crud.get_job(db, id=id, with_scans=True)
+    if db_job is None:
+        raise HTTPException(status_code=404, detail="Job not found")
+
+    return crud.get_scans_for_job(db, db_job)
 
 
 @router.patch(
@@ -75,15 +98,17 @@ def read_job(id: int, db: Session = Depends(get_db)):
 async def update_job(
     id: int, payload: schemas.JobUpdate, db: Session = Depends(get_db)
 ):
-
-    db_job = crud.get_job(db, id=id)
+    db_job = crud.get_job(db, id=id, with_scans=False)
     if db_job is None:
         raise HTTPException(status_code=404, detail="Job not found")
 
     (updated, job) = crud.update_job(db, id, payload)
 
-    if updated:
-        jobs = crud.get_jobs(db, scan_id=cast(int, job.scan_id))
-        await send_scan_event_to_kafka(ScanUpdateEvent(id=job.scan_id, jobs=jobs))
+    # TODO: make scan update event into job update event
+    # if updated:
+    #     jobs = crud.get_jobs(db, with_scans=True)
+    #     for job in jobs:
+    #         job.scans = crud.get_scans_for_job(db, job)
+    #     await send_scan_event_to_kafka(ScanUpdateEvent(id=job.scan_id, jobs=jobs))
 
     return job
