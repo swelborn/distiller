78a48579d362abb277d8df323a2944091c3b12b0
Sam Welborn <swelborn@lbl.gov>
2023-06-14 11:38:30 -0400

    backend: jobevent include event_type

--- backend/app/app/schemas/events.py
+++ backend/app/app/schemas/events.py
@@ -4,13 +4,23 @@ from pydantic import BaseModel
 
 from app.schemas.job import Job
 from app.schemas.scan import Scan
+from enum import Enum
 
 
 # Has to go in separate module rather the job.py because of circular import.
+class JobEventType(str, Enum):
+    SUBMIT = "job.submit"
+
+    def __str__(self) -> str:
+        return self.value
+
+    def __repr__(self) -> str:
+        return self.value
+
 class SubmitJobEvent(BaseModel):
     job: Job
     scan: Optional[Scan]
-
+    event_type = JobEventType.SUBMIT
 
 class RemoveScanFilesEvent(BaseModel):
     scan: Scan
