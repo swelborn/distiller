bbc9523f124fa7ac5ba33e87df364234120e7016
Sam Welborn <swelborn@lbl.gov>
2023-06-15 01:11:21 -0400

    add session page

---
+++ frontend/distiller/src/pages/session.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import ScansPage, { ScansPageProps } from './scans';
+import { getJobScans, scansSelector } from '../features/scans';
+import { useParams as useUrlParams, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import IconButton from '@mui/material/IconButton';
+import { IdType } from '../types';
+
+export interface SessionPageProps {
+  jobId?: IdType;
+}
+
+const SessionPage: React.FC<SessionPageProps> = ({ jobId: initialJobId }) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const jobIdParam = useUrlParams().jobId;
+  const jobId = initialJobId || parseInt(jobIdParam as string, 10);
+
+  useEffect(() => {
+    if ({initialJobId}) {
+        return;
+    }
+    dispatch(getJobScans({ jobId: jobId }));
+  }, [dispatch, jobId]);
+
+  const scansPageProps = {
+    selector: scansSelector.selectAll,
+    showScansToolbar: false,
+    showTablePagination: false,
+    showDiskUsage: false,
+    shouldFetchScans: false,
+  };
+  return (
+    <React.Fragment>
+      <IconButton onClick={() => navigate(-1)} color="primary">
+        <ArrowBackIcon />
+      </IconButton>
+      <ScansPage {...scansPageProps}></ScansPage>
+    </React.Fragment>
+  );
+};
+
+export default SessionPage;
