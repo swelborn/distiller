0ab375ee64e1370f69d18ac49401071897abd461
Sam Welborn <swelborn@lbl.gov>
2023-06-19 22:36:17 -0400

    sessions page, new draft

--- frontend/distiller/src/pages/sessions.tsx
+++ frontend/distiller/src/pages/sessions.tsx
@@ -1,34 +1,33 @@
 // External dependencies
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
 import { isNil, groupBy } from 'lodash';
 import { DateTime } from 'luxon';
-import { styled } from '@mui/material/styles';
+import { Add } from '@mui/icons-material';
 import {
   Box,
-  Button,
-  Card,
-  CardContent,
   List,
-  ListItem,
   TablePagination,
-  Typography,
   Divider,
   Switch,
   FormControlLabel,
   Chip,
+  Fab,
+  ListItem,
 } from '@mui/material';
 import useLocalStorageState from 'use-local-storage-state';
 
 // Internal dependencies
 import {
-  allJobsSelector,
   getJobs,
   totalCount as totalJobCount,
-  patchJob,
+  selectJobsByPage,
+  selectJobsByDate,
+  fetchedJobIdsSelector,
+  addFetchedJobId,
+  setSessionJobId,
 } from '../features/jobs';
 import { createJob } from '../features/jobs/api';
-import { getJobScans, scansSelector } from '../features/scans';
+import { getJobScans } from '../features/scans';
 import {
   getMachineState,
   machineSelectors,
@@ -38,120 +37,22 @@ import {
   microscopesSelectors,
   microscopesState,
 } from '../features/microscopes';
-import { Serializer, Deserializer, useUrlState } from '../routes/url-state';
+import { useUrlState } from '../routes/url-state';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { IdType, Job, JobType, Microscope } from '../types';
+import { IdType, Job, JobType } from '../types';
 import { canRunJobs } from '../utils/machine';
-import { canonicalMicroscopeName } from '../utils/microscopes';
 import StreamingDialog from '../components/streaming-dialog';
-import SessionPage from './session';
-import EditableField from '../components/editable-field';
-
-// Styles for HoverCard
-const HoverCard = styled(Card)({
-  width: '100%',
-  transition: 'transform 0.15s ease-in-out',
-  boxShadow: '0 4px 6px 0 hsla(0, 0%, 0%, 0.2)',
-  '&:hover': {
-    transform: 'scale3d(1.05, 1.05, 1)',
-  },
-});
-
-// Properties for MemoListItem
-interface MemoListItemProps {
-  job: Job;
-  setHoveredJobId: React.Dispatch<React.SetStateAction<IdType | null>>;
-  hoveredJobId: IdType | null;
-  enablePreview: boolean;
-  notesValue: string;
-  onSaveNotes: (value: string) => Promise<any>;
-}
-
-// MemoListItem Component
-const MemoListItem = React.memo(
-  ({
-    job,
-    setHoveredJobId,
-    hoveredJobId,
-    enablePreview,
-    notesValue,
-    onSaveNotes,
-  }: MemoListItemProps) => {
-    const navigate = useNavigate();
-
-    const handleMouseEnter = (event: React.MouseEvent<HTMLElement>) => {
-      setHoveredJobId(job.id);
-    };
-
-    const handleMouseLeave = () => {
-      setHoveredJobId(null);
-    };
-
-    return (
-      <ListItem key={job.id} style={{ width: '100%' }}>
-        <HoverCard
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          onClick={() => navigate(`${job.id}`)}
-        >
-          <CardContent>
-            <Typography variant="h5" component="div">
-              {job.id}
-            </Typography>
-            <Typography variant="h6" component="div">
-              {job.submit
-                ? DateTime.fromISO(job.submit).toLocaleString(
-                    DateTime.TIME_SIMPLE
-                  )
-                : ''}
-            </Typography>
-            <EditableField value={notesValue} onSave={onSaveNotes} />
-            {enablePreview && hoveredJobId === job.id && (
-              <SessionPage
-                jobId={hoveredJobId}
-                showBackButton={false}
-              ></SessionPage>
-            )}
-          </CardContent>
-        </HoverCard>
-      </ListItem>
-    );
-  }
-);
-
-export const intSerializer: Serializer<number> = (n) => {
-  if (isNil(n)) {
-    return '';
-  } else {
-    return n.toString();
-  }
-};
-
-export const intDeserializer: Deserializer<number> = (nStr) => {
-  const n = parseInt(nStr);
-
-  if (!Number.isFinite(n)) {
-    return undefined;
-  }
-
-  return n;
-};
+import { dateTimeDeserializer, dateTimeSerializer } from './scans';
+import { ScansToolbar } from '../components/scans-toolbar';
+import { intDeserializer, intSerializer } from './scans';
+import SessionCard from '../components/session-card';
 
 const SessionsPage: React.FC = () => {
+  // Job type to display (always streaming)
+  let jobType = JobType.Streaming;
+
   // Redux Hooks
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-
-  // Selectors
-  const jobs = useAppSelector(allJobsSelector);
-  const scans = useAppSelector(scansSelector.selectAll);
-  const totalJobs = useAppSelector(totalJobCount);
-  const microscopes = useAppSelector((state) =>
-    microscopesSelectors.selectAll(microscopesState(state))
-  );
-  const machines = useAppSelector((state) =>
-    machineSelectors.selectAll(machineState(state))
-  );
 
   // URL state management for page and rowsPerPage
   const [page, setPage] = useUrlState(
@@ -167,38 +68,55 @@ const SessionsPage: React.FC = () => {
     intDeserializer
   );
 
+  // URL state management for date filters
+  const [startDateFilter, setStartDateFilter] = useUrlState<DateTime | null>(
+    'startDate',
+    null,
+    dateTimeSerializer,
+    dateTimeDeserializer
+  );
+
+  const [endDateFilter, setEndDateFilter] = useUrlState<DateTime | null>(
+    'endDate',
+    null,
+    dateTimeSerializer,
+    dateTimeDeserializer
+  );
+
+  // Selectors
+  const totalJobs = useAppSelector(totalJobCount);
+  const microscopes = useAppSelector((state) =>
+    microscopesSelectors.selectAll(microscopesState(state))
+  );
+  const machines = useAppSelector((state) =>
+    machineSelectors.selectAll(machineState(state))
+  );
+  const fetchedJobIds = useAppSelector(fetchedJobIdsSelector);
+  const jobs = useAppSelector(
+    startDateFilter || endDateFilter
+      ? selectJobsByDate(startDateFilter, endDateFilter, jobType)
+      : selectJobsByPage(page, rowsPerPage, jobType)
+  );
+
   // Job-related state management
   const [hoveredJobId, setHoveredJobId] = useState<IdType | null>(null);
-  const [sessionJobId, setSessionJobId] = useState<IdType | null>(null);
   const [enablePreview, setEnablePreview] = useState<boolean>(true);
   const jobsGroupedByDate = groupBy(jobs, (job) =>
     job.submit ? DateTime.fromISO(job.submit).toISO().split('T')[0] : undefined
   );
 
   // Machine-related state management
-  const machineNames = machines.map((machine) => machine.name);
   const [machine, setMachine] = useLocalStorageState<string>('machine', {
     defaultValue: machines.length > 0 ? machines[0].name : '',
   });
 
   // Microscope-related state management
-  let microscope: Microscope | null = null;
-  const microscopesByCanonicalName = microscopes.reduce(
-    (obj: { [key: string]: Microscope }, microscope) => {
-      obj[canonicalMicroscopeName(microscope.name)] = microscope;
-      return obj;
-    },
-    {}
-  );
   let microscopeId: IdType | undefined =
     microscopes.length > 0 ? microscopes[0].id : undefined;
 
   // Streaming and session related state management
-  const [showScansPage, setShowScansPage] = useState<boolean>(false);
-  const [startStreaming, setStartStreaming] = useState<boolean>(false);
   const [startSessionDisabled, setStartSessionDisabled] =
     useState<boolean>(false);
-  const [endSessionDisabled, setEndSessionDisabled] = useState<boolean>(true);
   const [jobDialog, setJobDialog] = useState<JobType | undefined>();
 
   // Event Handlers
@@ -214,16 +132,7 @@ const SessionsPage: React.FC = () => {
     setPage(0);
   };
   const handleStartSessionClick = () => {
-    setShowScansPage(true);
-    setStartStreaming(true);
-    setEndSessionDisabled(false);
     setStartSessionDisabled(true);
-  };
-  const handleEndSessionClick = async () => {
-    setShowScansPage(false);
-    setStartStreaming(false);
-    setEndSessionDisabled(true);
-    setStartSessionDisabled(false);
   };
   const onStartStreamingClick = () => {
     fetchMachineStates();
@@ -233,9 +142,26 @@ const SessionsPage: React.FC = () => {
   const onJobSubmit = async (type: JobType, machine: string, params: any) => {
     handleStartSessionClick();
     const returnedJob: Job = await createJob(type, null, machine, params);
-    setSessionJobId(returnedJob.id);
+    if (returnedJob) {
+      dispatch(setSessionJobId(returnedJob.id));
+    }
     return returnedJob;
   };
+  const onStartDate = useCallback(
+    (date: DateTime | null) => {
+      setPage(0);
+      setStartDateFilter(date);
+    },
+    [setPage, setStartDateFilter]
+  );
+  const onEndDate = useCallback(
+    (date: DateTime | null) => {
+      setPage(0);
+      setEndDateFilter(date);
+    },
+    [setPage, setEndDateFilter]
+  );
+
   const handleTogglePreview = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEnablePreview(event.target.checked);
   };
@@ -251,23 +177,28 @@ const SessionsPage: React.FC = () => {
   };
   const fetchMachineStates = () =>
     machines.forEach((machine) => dispatch(getMachineState(machine)));
-  const onSaveNotes = (id: IdType, notes: string) => {
-    return dispatch(patchJob({ id, updates: { notes } }));
-  };
 
   // Effects
   useEffect(() => {
-    if (hoveredJobId !== null && enablePreview)
+    if (
+      hoveredJobId !== null &&
+      enablePreview &&
+      !fetchedJobIds.includes(hoveredJobId)
+    ) {
       dispatch(getJobScans({ jobId: hoveredJobId }));
-  }, [dispatch, hoveredJobId]);
+      dispatch(addFetchedJobId(hoveredJobId));
+    }
+  }, [dispatch, hoveredJobId, enablePreview, fetchedJobIds]);
 
   useEffect(() => {
     if (microscopeId === undefined) return;
     dispatch(
       getJobs({
-        withScans: false,
         skip: page * rowsPerPage,
         limit: rowsPerPage,
+        jobType: jobType,
+        start: startDateFilter || undefined,
+        end: endDateFilter || undefined,
       })
     );
   }, [dispatch, page, rowsPerPage]);
@@ -280,38 +211,21 @@ const SessionsPage: React.FC = () => {
 
   return (
     <React.Fragment>
-      <FormControlLabel
-        control={
-          <Switch
-            checked={enablePreview}
-            onChange={handleTogglePreview}
-            color="primary"
-          />
-        }
-        label="Enable Preview"
-      />
       <Box
         sx={{ display: 'flex', justifyContent: 'center', '& button': { m: 2 } }}
       >
         <div>
-          <Button
-            disabled={startSessionDisabled}
+          <Fab
             color="primary"
-            variant="contained"
+            aria-label="start"
+            disabled={startSessionDisabled}
             onClick={onStartStreamingClick}
           >
-            Start New Streaming Session
-          </Button>
-          <Button
-            disabled={endSessionDisabled}
-            color="error"
-            variant="contained"
-            onClick={handleEndSessionClick}
-          >
-            End Current Streaming Session
-          </Button>
+            <Add />
+          </Fab>
         </div>
       </Box>
+
       {Object.entries(jobsGroupedByDate)
         .sort(
           ([dateA], [dateB]) =>
@@ -319,32 +233,65 @@ const SessionsPage: React.FC = () => {
         )
         .map(([date, jobs], index, arr) => (
           <Box key={date} mb={3}>
-            <Chip
-              label={date}
-              variant="filled"
-              color="primary"
+            <Box
               sx={{
-                height: 'auto',
-                padding: '15px',
-                fontSize: '1.2rem',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
               }}
-            />
+            >
+              <Chip
+                label={date}
+                variant="filled"
+                color="primary"
+                sx={{
+                  height: 'auto',
+                  padding: '15px',
+                  fontSize: '1.2rem',
+                }}
+              />
+              {index === 0 && (
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={enablePreview}
+                        onChange={handleTogglePreview}
+                        color="primary"
+                      />
+                    }
+                    label="Enable Preview"
+                  />
+                  <ScansToolbar
+                    startDate={startDateFilter}
+                    endDate={endDateFilter}
+                    onStartDate={onStartDate}
+                    onEndDate={onEndDate}
+                    showFilterBadge={
+                      !isNil(startDateFilter) || !isNil(endDateFilter)
+                    }
+                    showExportButton={false}
+                  />
+                </Box>
+              )}
+            </Box>
             <List style={{ width: '100%' }}>
               {jobs.map((job) => (
-                <MemoListItem
-                  job={job}
-                  setHoveredJobId={setHoveredJobId}
-                  hoveredJobId={hoveredJobId}
-                  enablePreview={enablePreview}
-                  notesValue={job.notes || ''}
-                  onSaveNotes={(notesValue) => onSaveNotes(job.id, notesValue)}
-                />
+                <ListItem key={job.id} style={{ width: '100%' }}>
+                  <SessionCard
+                    key={job.id}
+                    job={job}
+                    setHoveredJobId={setHoveredJobId}
+                    showScans={enablePreview}
+                    isHoverable={true}
+                    compactMode={true}
+                  />
+                </ListItem>
               ))}
             </List>
             {index < arr.length - 1 && <Divider sx={{ height: '2px' }} />}
           </Box>
         ))}
-
       <TablePagination
         rowsPerPageOptions={[10, 20, 100]}
         component="div"
